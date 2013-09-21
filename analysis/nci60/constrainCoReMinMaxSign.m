565f9ad65dc741871f5e3b5c369aa15b62fc27fc
brandon <brandon.barker@gmail.com>
2013-09-20 20:01:06 -0400

    first working version of constrainCoReMinMaxSign.m

--- analysis/nci60/constrainCoReMinMaxSign.m
+++ analysis/nci60/constrainCoReMinMaxSign.m
@@ -5,7 +5,7 @@ function modelConstrained = constrainCoReMinMaxSign(model)
 
 % Brandon Barker    09/20/2013
 
-[celllinesarray jainMetsArray coretable] = readJainTable();
+[celllinesarray jainMetsArray coretable] = readJainTable(1);
 coretableMin = min(coretable');
 coretableMax = max(coretable');
 jainMetsToExcIdxs = loadJainMetsToExcIdxs(jainMetsArray, model);
@@ -17,18 +17,26 @@ for i = 1:length(modelConstrained.rxns)
     if (~isempty(regexp(modelConstrained.rxns{i}, '^EX(.)*\(e\)$')))
         foundInJainMetsToExcIdxs = 0;
 	for k = 1:length(jainMetsArray)
-	    kthExcIdxs = jainMetsToExcIdxs(jainMetsArray{k})
+	    kthExcIdxs = jainMetsToExcIdxs(jainMetsArray{k});
+            for j = 1:length(kthExcIdxs)
+                rxnIdx = kthExcIdxs(j);
+                rxn = modelConstrained.rxns(rxnIdx);
+                jmet = jainMetsArray{k};
+		if coretableMin(k) > 0
+                    modelConstrained.lb(rxnIdx) = 0;
+                    % Should we unconstrain with such large bounds?
+                    modelConstrained.ub(rxnIdx) = 1000;
+                    modelConstrained.rev(rxnIdx) = 0;
+                elseif coretableMax(k) < 0
+                    modelConstrained.lb(rxnIdx) = -1000;
+                    modelConstrained.ub(rxnIdx) = 0;
+                    modelConstrained.rev(rxnIdx) = 0;
+                end
+            end
 	    if(sum(kthExcIdxs == j) ~= 0)
 		foundInJainMetsToExcIdxs = 1;
 	    end
 	end
-%	disp (foundInJainMetsToExcIdxs)
-        if foundInJainMetsToExcIdxs
-	    disp(size(find(kthExcIdxs)))
-	    
-            %modelConstrained.lb(i)
-            %modelConstrained.ub(i)
-        end
         %For now don't constrain other reactions.
         %if (sum(mediumExcIdxs == i) == 0 && ~foundInJainMetsToExcIdxs)
         %    modelConstrained.lb(i) = 0;
