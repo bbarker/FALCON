6a6ecf733c5e7fa859a27ebff265d02caa01c1ad
brandon <brandon.barker@gmail.com>
2013-12-14 22:20:33 -0500

    added human models

--- analysis/timingAnalysis.m
+++ analysis/timingAnalysis.m
@@ -19,13 +19,18 @@ function [timeMat iterMat] = timingAnalysis( nReps)
 % best to have an optional argument to load these all from a .mat file
 
 % Note iIN800 and iFF708 don't appear to have valid Gene labels
-yeastModFiles = {'iND750.xml', 'iMM904_flux_orig.xml', ...
-                 'yeast_5.21_MCISB.xml', 'yeast_6.06_cobra.xml', ...
-                 'yeast_7.00_cobra.xml'};
+yeastModFiles = {'iND750', 'iMM904_flux_orig', 'yeast_5.21_MCISB', ...
+                 'yeast_6.06_cobra', 'yeast_7.00_cobra'};
 
 %include Ecoli core model
-ecoliModFiles = {'ecoli_core_model.xml', 'iJR904.xml', 'iAF1260.xml' ...
-                 'JO1366.xml' }
+ecoliModFiles = {'ecoli_core_model', 'iJR904', 'iAF1260', 'JO1366' }
+
+% ? include recon 2.1 if on time?
+humanModFiles = {'recon1', 'recon2model.v02'};
+%We can use a set of tissue specific models for human recon 1 and 2
+%but this might be overkill for the original implementation,
+%and may introduce other complications from tissue-specific model
+%creation
 
 % Now save to .mat file if .mat not provided as an argument.
 
